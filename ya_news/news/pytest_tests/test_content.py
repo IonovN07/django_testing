369d98f251612f6ac6f806491e5edf0bbb3959e3
Nik Ionov <nhjase@yandex.ru>
2025-03-17 23:42:06 +0500

    Correct tests after review

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -9,14 +9,8 @@ DETAIL_URL = pytest.lazy_fixture('detail_url')
 
 pytestmark = pytest.mark.django_db
 
-# @pytest.mark.parametrize(
-#     'url',
-#     [
-#         DETAIL_URL,
-#     ],
-# )
-# def get_news_detail_url():
-#     return url
+def parametrize_with_detail_url(test_func):
+    return pytest.mark.parametrize('url', [DETAIL_URL])(test_func)
 
 @pytest.mark.parametrize(
     'url',
@@ -28,21 +22,21 @@ def test_news_count(client, sample_news, url):
     response = client.get(url)
     assert len(response.context['object_list']) == NEWS_PER_PAGE
 
+@parametrize_with_detail_url
+def test_comments_order_on_news_page(client, sample_comments, url):
+    timestamps = [
+    comment.created 
+    for comment in client.get(url).context['news'].comment_set.all()
+    ]
+    assert timestamps == sorted(timestamps)
 
-# def test_comments_order_on_news_page(client, create_comments, news):
-#     response = client.get(get_news_detail_url(news))
-#     assert response.status_code == HTTPStatus.OK
-#     news_comments = response.context['news'].comment_set.all()
-#     timestamps = [comment.created for comment in news_comments]
-#     assert timestamps == sorted(timestamps)
+@parametrize_with_detail_url
+def test_anonymous_client_has_no_form(client, url):
+    assert 'form' not in client.get(url).context
 
-
-# def test_anonymous_client_has_no_form(client, news):
-#     response = client.get(get_news_detail_url(news))
-#     assert 'form' not in response.context
-
-
-# def test_authorized_client_has_form(not_author_client, news):
-#     response = not_author_client.get(get_news_detail_url(news))
-#     assert 'form' in response.context
-#     assert isinstance(response.context['form'], CommentForm)
+@parametrize_with_detail_url
+def test_authorized_client_has_form(not_author_client, url):
+    assert (
+        not_author_client.get(url).context.get('form') is not None and 
+        isinstance(not_author_client.get(url).context.get('form'), CommentForm)
+    )
